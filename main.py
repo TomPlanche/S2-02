fd8acf38c2a95e701e2262c3ff2f4c59f13da78b
Angel0503 <angel.ag64100@gmail.com>
2022-05-21 12:31:03 +0200

    Floyd sans remontee + debut astar

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ Created on Wed Mar 30 14:28:00 2022
 @author: angel
 """
 
+
 import json # Pour gérer le fichier donneesbus.json
-from math import sin, cos, acos, pi
+from math import sin, cos, acos, pi, sqrt
 from copy import deepcopy
+
+
+from ressources import graphics as gr
+
+
 
 with open("Fichiers/donneesBus.json") as fic_donnees_bus:
     donneesBus = json.load(fic_donnees_bus)
@@ -75,11 +81,13 @@ def voisin(nom_som: str) -> list:
 mat_bus = [
     [1 if nom_som in voisin(nom_som1) else 0 for nom_som in noms_arrets] for nom_som1 in noms_arrets
 ]
+print(mat_bus)
 
 # Création de la liste d'adjacence sous forme d'un dictionnaire.
 dict_bus = {
     nom_arret: voisin(nom_arret) for nom_arret in noms_arrets
 }
+print(dict_bus)
 
 
 def distanceGPS(latA: float, latB: float, longA: float, longB: float) -> float:
@@ -143,7 +151,7 @@ def distance_arc(arret1: str, arret2: str) -> float:
 poids_bus = [
     [distance_arc(nom_som1, nom_som2) for nom_som2 in noms_arrets] for nom_som1 in noms_arrets
 ]
-#print(poids_bus)
+print(poids_bus)
 """
 Algortihme de Bellman
 Deux conditions d'arrêts :
@@ -214,7 +222,7 @@ def bellman(arret_dep: str, arret_arriv: str) -> tuple:
 # print(bellman(noms_arrets[1], voisin(noms_arrets[1])[0]))
 # print(distance_arrets(noms_arrets[1], voisin(noms_arrets[1])[0]))
 #a = bellman(noms_arrets[1], voisin(noms_arrets[1])[0])
-print(bellman("STLE", "BRNM"))
+#print(bellman("STLE", "BRNM"))
 
 
 def floydWarshall(arret_dep, arret_arriv):
@@ -283,16 +291,134 @@ def floydWarshall(arret_dep, arret_arriv):
     print("======================")
     """
 
-   #Faire la remontée de la matrice
+    #Faire la remontée de la matrice
+   
+    #BUG DE LE REMONTEE
+    
     listeArrets = []
-    listeArrets.append(arret_arriv)
     sommet = arret_arriv
+    listeArrets.append(sommet)
     while sommet != arret_dep:
-        sommet = matricePred[noms_arrets.index(sommet)][noms_arrets.index(arret_dep)]
-        listeArrets.append(sommet)
+        listeArrets.append(matricePred[indice_som(sommet)][indice_som(arret_dep)])
+        sommet = matricePred[indice_som(sommet)][indice_som(arret_dep)]
     listeArrets = [arret_dep] + listeArrets
+    listeArrets.reverse()
     print(f"Pour aller de {arret_dep} à {arret_arriv}, il y a {matricePoids[noms_arrets.index(arret_arriv)][noms_arrets.index(arret_dep)]} et il faut passer par les arrêts {listeArrets}.")
     
+print(floydWarshall("STLE", "BRNM"))
+
+#poids_bus : valeur a vol  d'oiseau entre l'arret et ses voisins
+#dict_bus : arret_courant : voisin1,voisin2
+
+def calculHeuristique(arret_courant, arret_arriv):
+    return sqrt(arret_courant[])
+
+def astart(arret_dep, arret_arriv):
+    """
+    Variables
+    g = distance entre l'arrêt de départ et l'arrêt actuellement traité
+    h = distance heuristique entre l'arrêt actuel et l'arrêt d'arrivé
+    f = somme de g et h permettant de déterminer le chemin le plus rapide
+    """
+    
+    #Initialiser la liste ouverte (contient les arrêts voisin de l'arrêt actuellement analyser que l'on doit encore traité)
+    liste_ouverte = [arret_dep]
+    
+    #Initialiser la liste fermée (contient les arrêts déjà traités)
+    liste_fermee = []
+    
+    #Arret actuellement traité
+    arret_courant = arret_dep
     
     
-#print(floydWarshall(noms_arrets[1], voisin(noms_arrets[1])[0]))
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+#longitude ->
+longitudeMax = 43.55256
+longitudeMin = 43.430492
+
+
+#lattitude |
+#          v 
+lattitudeMax = abs(-1.415720)
+lattitudeMin = abs(-1.598933)
+
+diffLongitude =  0.12206799999999873
+diffLattitude =  0.18321299999999985
+
+print(longitudeMax/2/diffLongitude*2.5)
+print(lattitudeMin/2/diffLattitude*62.5)
+
+
+def testGraphique():
+    window = gr.GraphWin("Test", 900,500)
+    
+    testImage = gr.Image(gr.Point(450,250), "mapBus.png")
+    testImage.draw(window)
+    
+    
+    c1 = gr.Circle(gr.Point(43.55256,-1.415720),4)
+    
+    c2 = gr.Circle(gr.Point(43.430492,-1.598933),4)
+    
+    c3 = gr.Circle(gr.Point((longitudeMax/2/diffLongitude*2.5),(lattitudeMin/2/diffLattitude*62.5)),4)
+    
+    c1.setOutline("red")
+    c1.draw(window)
+    
+    c2.draw(window)
+    
+    c3.draw(window)
+    
+    line = gr.Line(c1.getCenter(), c2.getCenter())
+    line.draw(window)
+    
+    window.getMouse()
+    window.close()
+
+#testGraphique()
+
+def tousLesPoints():
+    window = gr.GraphWin("Test", 900,500)
+    
+    testImage = gr.Image(gr.Point(450,250), "mapBus.png")
+    testImage.draw(window)
+    
+    for arret in donneesBus:
+        for x,y,arrets in donneesBus[arret]:
+            cercle = gr.Circle(gr.Point((x/2/diffLongitude*2.5),(y/2/diffLattitude*62.5)),4)
+            cercle.draw(window)
+            
+    window.getMouse()
+    window.close()
+    
+    print([donneesBus[arret] for arret in donneesBus][0])
+
+#tousLesPoints()
+"""
